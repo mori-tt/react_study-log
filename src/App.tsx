d86c620f99d63ff608ce6a33dd87e388e5d767f3
T_Mori <morit0301@gmail.com>
2025-01-12 17:52:10 +0900

    Display of statistics by category, implementation of learning log addition form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
-import { INITIAL_LOGS } from "./constants";
-import { StudyLog } from "./types";
+import { useCallback, useState } from "react";
+import CategoryData from "./components/CategoryData";
+import StudyForm from "./components/StudyLogForm";
 import StudyLogList from "./components/StudyLogList";
 import TotalAnalytics from "./components/TotalAnalytics";
+import { INITIAL_LOGS } from "./constants";
+import { StudyLog } from "./types";
 
 function App() {
   const [logs, setLogs] = useState<StudyLog[]>(INITIAL_LOGS);
 
-  const addLog = (log: Omit<StudyLog, "id">) => {
+  // ログの追加処理をメモ化
+  const addLog = useCallback((log: Omit<StudyLog, "id">) => {
     setLogs((prev) => [
       ...prev,
       {
@@ -15,7 +18,7 @@ function App() {
         id: crypto.randomUUID(),
       },
     ]);
-  };
+  }, []);
 
   const deleteLog = (id: string) => {
     setLogs((prev) => prev.filter((log) => log.id !== id));
@@ -24,7 +27,6 @@ function App() {
   return (
     <>
       <div className="min-h-screen  pt-16 pb-8 space-y-12 px-16 bg-gradient-to-br from-blue-100 to-green-100">
-        {/* header */}
         <header className="space-y-8 flex flex-col items-center">
           <h1 className="text-5xl font-bold">Weekly Study Log</h1>
           <p className="text-gray-700 text-center">
@@ -35,12 +37,13 @@ function App() {
         </header>
         <main className="flex gap-12">
           <div className="w-3/4 h-full space-y-6">
-            {/* 3つのカード */}
-            {/* グラフ */}
+            <CategoryData logs={logs} />
             <TotalAnalytics logs={logs} />
             <StudyLogList logs={logs} onDelete={deleteLog} />
           </div>
-          <div className="w-1/4 h-full">{/* ログ入力フォーム */}</div>
+          <div className="w-1/4 h-full">
+            <StudyForm onSubmit={addLog} />
+          </div>
         </main>
       </div>
     </>
